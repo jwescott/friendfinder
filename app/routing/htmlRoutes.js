f8bd4f719f382080184965f33f5a57ef2e54be7d
jwescott <jwescott@mail.usf.edu>
2020-02-29 09:11:10 -0500

    added app folder

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,10 @@
+var path = require("path")
 function htmlRoutes(app) {
-    
+    app.get("/",function(req,res){
+
+        res.sendFile(path.join(__dirname,"../public/home.html"))
+
+    })
 }
 
 module.exports = htmlRoutes
